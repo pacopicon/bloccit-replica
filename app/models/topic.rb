43dc7c58da3a68aa1f04a9806e1f213b62d330c5
Paco <palmtreerooskee@gmail.com>
2015-11-10 21:11:22 -0600

    assignment-52-private-topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,7 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   self.per_page = 2
 
-  scope :visible_to, -> (user) {user ? all : where(public: true)}
+  scope :visible_to, -> (user) {user ? all : publicly_viewable}
+  scope :publicly_viewable, -> {where(public: true)}
+  scope :privately_viewable, -> {where(public: false)}
 end
