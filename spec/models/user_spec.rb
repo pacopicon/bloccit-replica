b68bf257093c80d98b3c497e370eebfbed8f2177
Paco <palmtreerooskee@gmail.com>
2015-11-10 20:17:27 -0600

    assignment 51 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,20 @@ describe User do
   include TestFactories
 
   describe "#favorited(post)" do
-    
-    xit "returns 'nil' if the user has not favorited the post" do
+
+    before do
+      @post = associated_post
+      @user = authenticated_user
+
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect(@user.favorited(@post)).to be_nil
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      favorite = @user.favorites.create(post: @post)
+      expect(@user.favorited(@post)).to eq(favorite)
     end
 
   end
