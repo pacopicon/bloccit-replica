41bf754df154d681e178a5d6a060d026446d9c6c
Paco <palmtreerooskee@gmail.com>
2015-11-15 19:23:18 -0600

    assignment 54 complete

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -7,7 +7,7 @@ describe "Visiting profiles" do
   before do
     @user = authenticated_user
     @post = associated_post(user: @user)
-    @comment = Comment.new(user: @user, body: "this is just a test.")
+    @comment = Comment.new(user: @user, post: @post, body: "this is just a test.")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save!
   end
@@ -24,21 +24,21 @@ describe "Visiting profiles" do
     end
   end
 
-  # describe "user visiting own profile" do
-  #
-  #   before do
-  #   login_as(@user, :scope => :user)
-  #   end
-  #
-  #   it "shows profile" do
-  #     visit user_path(@user)
-  #     expect(current_path).to eq(user_path(@user))
-  # 
-  #     expect(page).to have_content(@user.name)
-  #     expect(page).to have_content(@post.title)
-  #     expect(page).to have_content(@comment.body)
-  #   end
-  #
-  # end
+  describe "user visiting own profile" do
+
+    before do
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
+
+  end
 
 end
