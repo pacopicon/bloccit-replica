94ed9d12799a50c21024720f8570bce68d366f5e
Paco <palmtreerooskee@gmail.com>
2015-11-15 19:01:42 -0600

    checkpoint 55 INCOMPLETE

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,70 @@
+class Topics::PostsController < ApplicationController
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was successfully deleted."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+  end
+
+  def show
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    authorize @post
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.topic = @topic
+    authorize @post
+    if @post.save
+      @post.create_vote
+      flash[:notice] = "Success!"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post.  Please try again!"
+      render :new
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(post_params)
+      flash[:notice] = "The post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post.  Please try again!"
+      render :edit
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
+
+end
--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -22,6 +22,23 @@ describe "Visiting profiles" do
       expect(page).to have_content(@post.title)
       expect(page).to have_content(@comment.body)
     end
+  end
+
+  describe "user visiting own profile" do
+
+    before do
+    login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect(page).to have_content(@user.name)
+      expect(page).to have_content(@post.title)
+      expect(page).to have_content(@comment.body)
+    end
 
   end
+
 end
